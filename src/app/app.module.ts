6f60796fd396ccbc9ce3133fec08ccca24c058b3
Andrea <tu@esempio.com>
2023-03-29 21:04:17 +0200

    progetto ultimato per pc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './features/header/header.component';
 import { FooterComponent } from './features/footer/footer.component';
 import { HomeComponent } from './features/home/home.component';
 import { SkillsComponent } from './features/skills/skills.component';
@@ -14,7 +13,6 @@ import { ContactsComponent } from './features/contacts/contacts.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     FooterComponent,
     HomeComponent,
     SkillsComponent,
