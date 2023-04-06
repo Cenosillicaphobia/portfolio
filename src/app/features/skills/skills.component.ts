a7169b6b2dd7b4634fbd4b78c1e25bad72a03cfd
Andrea <tu@esempio.com>
2023-04-06 11:52:17 +0200

    skills component aggiornato

--- src/app/features/skills/skills.component.ts
+++ src/app/features/skills/skills.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css']
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit{
+
+  set:any = '';
+  iconList = ['html', 'css', 'angular', 'webpack', 'javascript', 'bootstrap', 'git', 'github', 'npm', 'ng-zorro', 'rxjs', 'firebase'];
+
+  select(icon:string){
+    this.set = icon 
+  }
+
+  randomIcon(){
+    this.set = this.iconList[Math.floor(Math.random() * this.iconList.length)];
+  }
+
+  ngOnInit(): void {
+    setInterval(() => {this.randomIcon()}, 3000); 
+  }
+  
+
+
 
 }
